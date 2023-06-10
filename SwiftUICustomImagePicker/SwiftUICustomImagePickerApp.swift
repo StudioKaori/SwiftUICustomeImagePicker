c8f994cc07d7b245f57702be0e88ffa54caf7466
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2023-06-10 22:40:27 +0200

    Initial Commit

---
+++ SwiftUICustomImagePicker/SwiftUICustomImagePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomImagePickerApp.swift
+//  SwiftUICustomImagePicker
+//
+//  Created by Kaori Persson on 2023-06-10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomImagePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
